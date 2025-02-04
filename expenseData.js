769d0c82147a4b41b284fee475fd2c08b40728aa
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-04 16:42:46 -0500

    form data handled

---
+++ expenseData.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id :  crypto.randomUUID(),
+        title : 'Milk', 
+        category: 'Grocery',
+        amount: 6.99,
+    },
+    {
+        id :  crypto.randomUUID(),
+        title : 'Shirt', 
+        category: 'Clothes',
+        amount: 16.99,
+    },
+    {
+        id :  crypto.randomUUID(),
+        title : 'Vegetables', 
+        category: 'Grocery',
+        amount: 9.99,
+    },
+    {
+        id :  crypto.randomUUID(),
+        title : 'Electricity Bill', 
+        category: 'Bills',
+        amount: 6.99,
+    }
+]
---
+++ src/components/ExpenseForm.jsx
@@ -0,0 +1,45 @@
+
+
+export default function ExpenseForm({setExpenses}) {
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        const expense = {id: crypto.randomUUID(), ...getFormData(event.target)}
+        setExpenses((prevState) => [...prevState, expense ] )
+        event.target.reset();
+    }
+
+    const getFormData = (form) =>{
+        const formData = new FormData(form);
+        const data = {}
+        for (const [key, value] of formData.entries()){
+            data[key] = value
+        }
+        return data
+    }
+
+  return (
+    <form className="expense-form" onSubmit={handleSubmit}>
+          <div className="input-container">
+            <label htmlFor="title">Title</label>
+            <input id="title" name="title" />
+          </div>
+          <div className="input-container">
+            <label htmlFor="category">Category</label>
+            <select id="category" name="category">
+                  <option value="" hidden>Select Category</option>
+                  <option value="grocery">Grocery</option>
+                  <option value="clothes">Clothes</option>
+                  <option value="bills">Bills</option>
+                  <option value="education">Education</option>
+                  <option value="medicine">Medicine</option>
+                </select>
+          </div>
+          <div className="input-container">
+            <label htmlFor="amount">Amount</label>
+            <input id="amount" name="amount" />
+          </div>
+          <button className="add-btn">Add</button>
+        </form>
+  );
+}
