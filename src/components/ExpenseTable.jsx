e1a98600b0366af684f660cb2e86700825983be5
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-06 14:42:24 -0500

    sort logic for title added

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -13,7 +13,7 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
 
   const [menuPosition, setMenuPosition] = useState({});
   const [rowId, setRowId] = useState("");
-  const [sortAmount, setSortAmount] = useState(() => () => {})
+  const [sortCallback, setSortCallback] = useState(() => () => {})
 
   return (
     <>
@@ -33,7 +33,35 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
         }}>
         <thead>
           <tr>
-            <th>Title</th>
+          <th className="amount-column">
+              <div>
+                <span>Title</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  viewBox="0 0 384 512"
+                  className="arrow up-arrow"
+                  onClick={() => {
+                    setSortCallback(() => (a, b) => a.title.localeCompare(b.title))
+                  }}
+                >
+                  <title>Ascending</title>
+                  <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  viewBox="0 0 384 512"
+                  className="arrow down-arrow"
+                  onClick={() => {
+                    setSortCallback(() => (a, b) => b.title.localeCompare(a.title))
+                  }}
+                >
+                  <title>Descending</title>
+                  <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                </svg>
+              </div>
+            </th>
             <th>
               <select
                 onChange={(e) => setFilteredData(e.target.value.toLowerCase())}
@@ -55,7 +83,7 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
                   onClick={() => {
-                    setSortAmount(() => (a,b) => a.amount - b.amount)
+                    setSortCallback(() => (a,b) => a.amount - b.amount)
                   }}
                 >
                   <title>Ascending</title>
@@ -67,7 +95,7 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
                   onClick={() => {
-                    setSortAmount(() => (a,b) => b.amount - a.amount)
+                    setSortCallback(() => (a,b) => b.amount - a.amount)
                   }}
                 >
                   <title>Descending</title>
@@ -78,7 +106,7 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
           </tr>
         </thead>
         <tbody>
-          {filteredData.sort(sortAmount).map(({ id, title, category, amount }) => {
+          {filteredData.sort(sortCallback).map(({ id, title, category, amount }) => {
             return (
               <tr
                 key={id}
@@ -97,7 +125,7 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
           <tr>
             <th>Total</th>
             <th onClick={() => {
-              setSortAmount(() => () => {})
+              setSortCallback(() => () => {})
             }}> Reset sort</th>
             <th>$ {total.toFixed(2)}</th>
           </tr>
