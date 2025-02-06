ca30d1f746bc12695b1ea66bf24341437db6be0e
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-06 14:29:53 -0500

    sort method on amount

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -13,6 +13,7 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
 
   const [menuPosition, setMenuPosition] = useState({});
   const [rowId, setRowId] = useState("");
+  const [sortAmount, setSortAmount] = useState(() => () => {})
 
   return (
     <>
@@ -25,7 +26,11 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
         setExpense={setExpense}
         setEditingRowId={setEditingRowId}
       />
-      <table className="expense-table" onClick={() => setMenuPosition({})}>
+      <table className="expense-table" onClick={() =>{ 
+        if(menuPosition.left){
+            setMenuPosition({})
+          }
+        }}>
         <thead>
           <tr>
             <th>Title</th>
@@ -49,6 +54,9 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
+                  onClick={() => {
+                    setSortAmount(() => (a,b) => a.amount - b.amount)
+                  }}
                 >
                   <title>Ascending</title>
                   <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
@@ -58,6 +66,9 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
+                  onClick={() => {
+                    setSortAmount(() => (a,b) => b.amount - a.amount)
+                  }}
                 >
                   <title>Descending</title>
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
@@ -67,7 +78,7 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
           </tr>
         </thead>
         <tbody>
-          {filteredData.map(({ id, title, category, amount }) => {
+          {filteredData.sort(sortAmount).map(({ id, title, category, amount }) => {
             return (
               <tr
                 key={id}
@@ -85,7 +96,9 @@ export default function ExpenseTable({ expenses, setExpenses, setExpense, setEdi
           })}
           <tr>
             <th>Total</th>
-            <th></th>
+            <th onClick={() => {
+              setSortAmount(() => () => {})
+            }}> Reset sort</th>
             <th>$ {total.toFixed(2)}</th>
           </tr>
         </tbody>
